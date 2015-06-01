eb14f7b14970868ebfdd8b4ce31d268ce196d7ae
Prakal <pranav.89@gmail.com>
2015-05-31 19:20:06 -0700

    success in embedding youtube links in page

--- public/app/dashboard/dashboard.js
+++ public/app/dashboard/dashboard.js
@@ -4,7 +4,7 @@ angular.module('reverse.dashboard', [])
     // Allow same origin resource loads.
     'self',
     // Allow loading from our assets domain.  Notice the difference between * and **.
-    'https://youtube.com/embed/**'
+    'https://www.youtube.com/embed/**'
   ]);
 })
 .controller('DashController', function ($scope, $http, $window, $timeout, $q) {
