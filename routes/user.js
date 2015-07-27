a1552b650983f3b3e736286906ae10b89da8d5ff
Prakal <pranav.89@gmail.com>
2015-07-26 23:12:20 -0700

    beautify file

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,18 @@
-
-
 var express = require('express');
 var router = express.Router();
 
-var Student  = require('./../app/models/student');
-var Instructor  = require('./../app/models/instructor');
+var Student  				= require('./../app/models/student');
+var Instructor  		= require('./../app/models/instructor');
 
-var passport = require('passport');
-var path         = require('path');
-var DBQuery = require('../utils/dbQueries.js')
-var db = require('../app/config.js')
+var passport 				= require('passport');
+var path        		= require('path');
+var DBQuery 				= require('../utils/dbQueries.js')
+var db 							= require('../app/config.js')
 
-var studentRoute = require('./student');
+var studentRoute 		= require('./student');
 var instructorRoute = require('./instructor');
 
-var usernameRoute = require('./username');
+var usernameRoute 	= require('./username');
 
 router.get('/', function(req, res) {
 	console.log('req.url is', req.url);
@@ -22,4 +20,4 @@ router.get('/', function(req, res) {
 
 router.use('/:username', usernameRoute);
 
-module.exports = router;
+module.exports 			= router;
