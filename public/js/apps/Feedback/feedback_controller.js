55a03127b60aa4b1157dc244a24c4d5487310621
Prakal <pranav.89@gmail.com>
2015-08-08 19:13:13 -0700

    increment current class numbers for either discipline based on previous feedback approval

--- public/js/apps/Feedback/feedback_controller.js
+++ public/js/apps/Feedback/feedback_controller.js
@@ -10,40 +10,51 @@ define([
         showFeedback: function(params){
           var FeedView = null;
           require(["entities/feedback"],function(){
-            var fetchFeed = VirtualDojo.request("entities:feedback:get", params)
-              fetchFeed
-                .done(function(data){
-                  if (data) {
-                    var feedbackModel = new FeedbackModels.Feedback(data[0]);
+            var fetchFeed = VirtualDojo.request("entities:feedback:get", params);
+            fetchFeed
+              .done(function(data){
+                if (data) {
+                  var feedbackModel = new FeedbackModels.Feedback(data[0]);
+                  // increment the current class number to unlock relevant class in either Kendo or Qigong.
+                  if (feedbackModel.get('approved') === true){
+                    if (feedbackModel.get('Class').discipline_id === 1){
+                      UTConfig.currentKendoClass = feedbackModel.get('Class').classNum + 1;
+                    } else {
+                      UTConfig.currentQigongClass = feedbackModel.get('Class').classNum + 1;
+                    }
                   }
-                  FeedView = new FeedbackView.Feedback({model: feedbackModel});
-                  VirtualDojo.regions.main.show(FeedView);
-                });
+                  console.log('UT',feedbackModel);
+                }
+                FeedView = new FeedbackView.Feedback({model: feedbackModel});
+                VirtualDojo.regions.main.show(FeedView);
+              });
           })
         },
         postFeedback: function(params){
           var FeedView = null;
           require(["entities/feedback"],function(){
-            var fetchFeed = VirtualDojo.request("entities:feedback:post", params)
-              fetchFeed
-                .done(function(data){
-                 if (data) {
-                    var feedbackModel = new FeedbackModels.Feedback(data);
-                  }
-                  FeedView = new FeedbackView.Feedback({model: feedbackModel});
-                  VirtualDojo.navigate("/feedback/" + feedbackModel.get("id"));
-                  VirtualDojo.regions.main.show(FeedView);
-                });
+            var fetchFeed = VirtualDojo.request("entities:feedback:post", params);
+            fetchFeed
+              .done(function(data){
+               if (data) {
+                  var feedbackModel = new FeedbackModels.Feedback(data);
+                }
+                FeedView = new FeedbackView.Feedback({model: feedbackModel});
+                VirtualDojo.navigate("/feedback/" + feedbackModel.get("id"));
+                VirtualDojo.regions.main.show(FeedView);
+              });
           })
         },
         approveFeedback: function(params){
           require(["entities/feedback"],function(){
-            var approveFeed = VirtualDojo.request("entities:feedback:approve", params)
-              approveFeed
-                .done(function(data){
-                  console.log("approved!");
-                  VirtualDojo.trigger("show:dashboard");
-                });
+            var approveFeed = VirtualDojo.request("entities:feedback:approve", params);
+            console.log('params',params);
+            approveFeed
+              .done(function(data){
+                console.log('approval data',data);
+                console.log("approved!");
+                VirtualDojo.trigger("show:dashboard");
+              });
           })
         }
       };
